03b6f658b5fa3fbdff4e6852d3cf759816c41424
einsteinkibet <einsteinkibet0110@gmail.com>
2023-10-06 15:37:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Remove the 'main#main' DOM node
+document.querySelector('main#main').remove();
+
+// Create a newheader variable that points to an <h1> element
+const newHeader = document.createElement('h1');
+
+// the 'newHeader'id  of the new <h1> element  has an id of 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element with your name in place of YOUR-NAME
+newHeader.textContent = 'YOUR-NAME is the champion';
